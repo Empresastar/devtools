9767dcee8aa0d6da002b7c45ef9ea0c75f4644ae
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 14:59:28 -0300

    Update monaco-init.js

--- assets/css/js/monaco-init.js
+++ assets/css/js/monaco-init.js
@@ -1,1 +1,14 @@
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs' }});
 
+let editor;
+
+require(['vs/editor/editor.main'], function() {
+    editor = monaco.editor.create(document.getElementById('monaco-editor'), {
+        value: "",
+        language: 'html',
+        theme: 'vs-dark',
+        automaticLayout: true,
+        fontSize: 14
+    });
+    console.log("Monaco OK!");
+});
