469b65ef235f89a6a998db447f27aa085e4ab188
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 17:27:06 -0300

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,25 +1,17 @@
 const EditorModule = {
     instance: null,
-    
-    init(containerId) {
+    init(idDiv) {
         return new Promise((resolve) => {
             require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs' }});
             require(['vs/editor/editor.main'], () => {
-                this.instance = monaco.editor.create(document.getElementById(containerId), {
+                this.instance = monaco.editor.create(document.getElementById(idDiv), {
                     theme: 'vs-dark',
                     automaticLayout: true,
                     fontSize: 14,
-                    fontFamily: 'Fira Code, Consolas, monospace',
-                    minimap: { enabled: true }
+                    language: 'javascript' // Aqui define as cores
                 });
-                resolve(this.instance);
+                resolve();
             });
         });
-    },
-
-    setLanguage(filename) {
-        const ext = filename.split('.').pop();
-        const map = { html: 'html', css: 'css', js: 'javascript', json: 'json' };
-        monaco.editor.setModelLanguage(this.instance.getModel(), map[ext] || 'javascript');
     }
 };
