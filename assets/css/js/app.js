f64c9aa7d662f5762523c6bc45ff41f08a1f76c8
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 15:01:04 -0300

    Implement file scanning and dynamic UI updates

--- assets/css/js/app.js
+++ assets/css/js/app.js
@@ -1,1 +1,33 @@
+const btn = document.getElementById('btn-scan');
+const tree = document.getElementById('tree-content');
+let vfs = {};
 
+function addFile(name, lang, type) {
+    const div = document.createElement('div');
+    div.className = 'file';
+    div.innerText = name;
+    div.onclick = () => {
+        let content = (type === 'root') ? vfs[name].content : vfs[type][name].content;
+        editor.setValue(content);
+        monaco.editor.setModelLanguage(editor.getModel(), lang);
+        document.getElementById('tab-title').innerText = name;
+        if(lang === 'html') document.getElementById('preview-frame').srcdoc = content;
+    };
+    tree.appendChild(div);
+}
+
+btn.onclick = async () => {
+    const url = document.getElementById('url-input').value;
+    btn.innerText = "Carregando...";
+    const data = await scanURL(url);
+    if(data) {
+        vfs = data;
+        tree.innerHTML = "";
+        addFile('index.html', 'html', 'root');
+        Object.keys(vfs.css).forEach(f => addFile(f, 'css', 'css'));
+        Object.keys(vfs.js).forEach(f => addFile(f, 'javascript', 'js'));
+        editor.setValue(vfs['index.html'].content);
+        document.getElementById('preview-frame').srcdoc = vfs['index.html'].content;
+    }
+    btn.innerText = "Scan";
+};
