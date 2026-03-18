b51fdff07c57178c45f13bc3c9e51a2001c4000a
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 16:06:05 -0300

    Delete css directory

--- css/js/script.js
+++
@@ -1,9 +0,0 @@
-// O objetivo é fazer o botão mudar de cor, mas tem um erro aqui!
-const botao = document.querySelector("button");
-
-botao.addEventListener("click", () => {
-    // BUG: A variável 'corRubi' não foi definida! 
-    // Tentem definir ela juntos para o código parar de dar erro no console.
-    botao.style.backgroundColor = corRubi; 
-    console.log("Tentando mudar a cor...");
-});
