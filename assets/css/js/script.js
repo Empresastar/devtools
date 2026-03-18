7ca95a6ee8871178bfe38517fe8932086ad01f8d
Empresastar <gemadotopmatheus@gmail.com>
2026-03-18 14:16:45 -0300

    Delete assets directory

--- assets/css/js/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn-acao');
-const statusText = document.getElementById('status');
-
-btn.addEventListener('click', () => {
-    statusText.innerText = "O script funcionou e as pastas estão certas! ✅";
-    statusText.style.color = "#00ff88";
-});
