b1177ac00d673501eed6e9d13576d8fbd22adaeb
Empresastar <gemadotopmatheus@gmail.com>
2026-03-19 00:30:12 -0300

    Refactor editor initialization and data handling

--- js/main.js
+++ js/main.js
@@ -1,51 +1,35 @@
 window.onload = async () => {
     let isRemoteUpdate = false; 
 
-    try {
-        await EditorModule.init('monaco-editor');
-    } catch (e) {
-        console.error("Erro no Editor:", e);
-    }
+    // Tenta iniciar o editor na div 'monaco-editor'
+    await EditorModule.init('monaco-editor').catch(() => alert("Erro ao carregar editor"));
 
     const handleData = (data) => {
-        if (data.type === 'SYNC') {
+        if (data.type === 'SYNC' || data.type === 'FILE') {
             isRemoteUpdate = true;
+            if (data.type === 'FILE') {
+                EditorModule.setLanguage(data.name);
+                document.getElementById('active-filename').innerText = data.name;
+            }
             const pos = EditorModule.instance.getPosition();
             EditorModule.instance.setValue(data.content);
             EditorModule.instance.setPosition(pos);
-            setTimeout(() => { isRemoteUpdate = false; }, 50);
-        }
-        if (data.type === 'FILE') {
-            isRemoteUpdate = true;
-            EditorModule.instance.setValue(data.content);
-            EditorModule.setLanguage(data.name);
-            const nameDisplay = document.getElementById('active-filename');
-            if (nameDisplay) nameDisplay.innerText = data.name;
             setTimeout(() => { isRemoteUpdate = false; }, 50);
         }
     };
 
     P2PModule.init(handleData);
 
-    const btnFolder = document.getElementById('open-folder-btn');
-    if (btnFolder) btnFolder.onclick = () => FilesModule.openFolder();
+    document.getElementById('open-folder-btn').onclick = () => FilesModule.openFolder();
+    document.getElementById('create-file-btn').onclick = () => FilesModule.createFile();
+    document.getElementById('connect-btn').onclick = () => {
+        const id = document.getElementById('peer-id-input').value;
+        if(id) P2PModule.connect(id, handleData);
+    };
 
-    const btnCreate = document.getElementById('create-file-btn');
-    if (btnCreate) btnCreate.onclick = () => FilesModule.createFile();
-    
-    const btnConnect = document.getElementById('connect-btn');
-    if (btnConnect) {
-        btnConnect.onclick = () => {
-            const input = document.getElementById('peer-id-input');
-            if (input) P2PModule.connect(input.value, handleData);
-        };
-    }
-
-    if (EditorModule.instance) {
-        EditorModule.instance.onDidChangeModelContent(() => {
-            if (!isRemoteUpdate) {
-                P2PModule.send({ type: 'SYNC', content: EditorModule.instance.getValue() });
-            }
-        });
-    }
+    EditorModule.instance.onDidChangeModelContent(() => {
+        if (!isRemoteUpdate) {
+            P2PModule.send({ type: 'SYNC', content: EditorModule.instance.getValue() });
+        }
+    });
 };
